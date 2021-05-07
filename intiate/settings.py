f31c119f777ae1a1165474aaa2777887f60edd04
TaseenAnsari <63318687+TaseenAnsari@users.noreply.github.com>
2021-05-08 04:37:52 +0530

    Update settings.py

--- intiate/settings.py
+++ intiate/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR,'statics')
+    os.path.join(BASE_DIR,'statics/CSS')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfile')
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
